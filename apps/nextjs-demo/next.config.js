6996f9c337453a1f7406042b8f7fd652f4c2745d
Alexander Schwarzman <hello@sashathor.com>
2024-12-03 16:16:58 +0100

    fix(cicd): vercel setup

--- apps/nextjs-demo/next.config.js
+++ apps/nextjs-demo/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@trycourier/react-editor"],
+  experimental: {
+    externalDir: true,
+  },
 };
 
 module.exports = nextConfig;
