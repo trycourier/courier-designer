599e65e7838c30181022c62e606a07cb47b0fe9a
Alexander Schwarzman <hello@sashathor.com>
2024-12-17 13:00:32 +0100

    chore(variable): improvements

--- packages/react-editor/src/components/Editor/extensions/Variable/Variable.types.ts
+++ packages/react-editor/src/components/Editor/extensions/Variable/Variable.types.ts
@@ -1,9 +1,17 @@
 import { Editor, Range } from "@tiptap/core";
-import { SuggestionProps, SuggestionOptions } from "@tiptap/suggestion";
+import { SuggestionOptions, SuggestionProps } from "@tiptap/suggestion";
+
+interface Props {
+  editor: Editor;
+  range: Range;
+  clientRect: () => DOMRect | null;
+  items: string[];
+  command: (props: { editor: Editor; range: Range; props: string }) => void;
+}
 
 export interface VariableOptions {
   HTMLAttributes: Record<string, any>;
-  suggestion: Partial<SuggestionOptions<string>>;
+  suggestion: Partial<SuggestionOptions>;
 }
 
 export interface VariableCommandProps {
@@ -12,10 +20,29 @@ export interface VariableCommandProps {
   props: string;
 }
 
-export type VariableSuggestionProps = SuggestionProps<string>;
+export interface VariableSuggestionProps extends Props {
+  selected: number;
+}
 
 export interface VariableNodeOptions {
   HTMLAttributes?: Record<string, any>;
+}
+
+export interface VariableNodeAttributes {
+  id: string | null;
+}
+
+export interface VariableSuggestionsProps extends SuggestionProps {
+  items: string[];
+  command: (props: string) => void;
+  editor: Editor;
+  query: string;
+  selected: number;
+}
+
+export interface VariableNodeOptions {
+  HTMLAttributes?: Record<string, any>;
+  suggestion: Partial<SuggestionOptions>;
 }
 
 export interface VariableNodeAttributes {
--- packages/react-editor/src/components/Editor/extensions/Variable/suggestion.ts
+++ packages/react-editor/src/components/Editor/extensions/Variable/suggestion.ts
@@ -1,8 +1,13 @@
-import { Editor } from "@tiptap/core";
+import { Editor, Range } from "@tiptap/core";
 import { ReactRenderer } from "@tiptap/react";
 import tippy, { Instance as TippyInstance } from "tippy.js";
 import { VariableSuggestions } from "./VariableSuggestions";
-import { SuggestionOptions } from "@tiptap/suggestion";
+import {
+  SuggestionOptions,
+  SuggestionProps,
+  SuggestionKeyDownProps,
+} from "@tiptap/suggestion";
+import { VariableSuggestionsProps } from "./Variable.types";
 
 export const suggestion: Partial<SuggestionOptions> = {
   items: ({ query }: { query: string; editor: Editor }) => {
@@ -13,37 +18,40 @@ export const suggestion: Partial<SuggestionOptions> = {
   },
 
   char: "{{",
-
   allowSpaces: false,
   allowedPrefixes: null,
   startOfLine: false,
   decorationTag: "span",
   decorationClass: "variable-suggestion",
 
-  command: ({ editor, range, props }) => {
+  command: ({
+    editor,
+    range,
+    props,
+  }: {
+    editor: Editor;
+    range: Range;
+    props: string;
+  }) => {
     editor
       .chain()
       .focus()
       .deleteRange(range)
       .insertContent([
-        {
-          type: "variable",
-          attrs: {
-            id: props,
-          },
-        },
+        { type: "variable", attrs: { id: props } },
         { type: "text", text: " " },
       ])
       .run();
   },
 
   render: () => {
-    let component: ReactRenderer | null = null;
-    let popup: TippyInstance[] | null = null;
+    let component: ReactRenderer<VariableSuggestionsProps> | null = null;
+    let popup: TippyInstance | null = null;
+
     let selectedIndex = 0;
 
     return {
-      onStart: (props) => {
+      onStart: (props: SuggestionProps) => {
         component = new ReactRenderer(VariableSuggestions, {
           props: {
             ...props,
@@ -52,37 +60,43 @@ export const suggestion: Partial<SuggestionOptions> = {
           editor: props.editor,
         });
 
-        if (!props.clientRect) {
-          return;
-        }
+        if (!props.clientRect) return;
 
-        popup = tippy("body", {
-          getReferenceClientRect: props.clientRect,
+        popup = tippy(document.body, {
+          getReferenceClientRect: () => props.clientRect?.() || new DOMRect(),
           appendTo: () => document.body,
-          content: component.element,
+          content: component?.element,
           showOnCreate: true,
           interactive: true,
           trigger: "manual",
           placement: "bottom-start",
-        });
+        }) as TippyInstance;
       },
 
-      onUpdate(props) {
+      onUpdate(props: SuggestionProps) {
         component?.updateProps({
           ...props,
           selected: selectedIndex,
         });
 
-        if (!props.clientRect) {
-          return;
-        }
+        if (!props.clientRect) return;
 
-        popup?.[0].setProps({
-          getReferenceClientRect: props.clientRect,
-        });
+        if (popup) {
+          popup.setProps({
+            getReferenceClientRect: () => props.clientRect?.() || new DOMRect(),
+          });
+        }
       },
 
-      onKeyDown({ event, items }) {
+      onKeyDown(props: SuggestionKeyDownProps) {
+        const { event } = props;
+
+        // @TODO: refactor this
+        const items = (suggestion.items?.({
+          query: (component?.props as VariableSuggestionsProps).query || "",
+          editor: (component?.props as VariableSuggestionsProps).editor,
+        }) || []) as string[];
+
         if (event.key === "ArrowUp") {
           selectedIndex = (selectedIndex + items.length - 1) % items.length;
           component?.updateProps({ selected: selectedIndex });
@@ -91,6 +105,7 @@ export const suggestion: Partial<SuggestionOptions> = {
 
         if (event.key === "ArrowDown") {
           selectedIndex = (selectedIndex + 1) % items.length;
+          console.log(selectedIndex, component);
           component?.updateProps({ selected: selectedIndex });
           return true;
         }
@@ -106,7 +121,9 @@ export const suggestion: Partial<SuggestionOptions> = {
       },
 
       onExit() {
-        popup?.[0].destroy();
+        if (popup) {
+          popup.destroy();
+        }
         component?.destroy();
       },
     };
