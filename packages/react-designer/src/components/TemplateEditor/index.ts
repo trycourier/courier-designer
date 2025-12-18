469bedd8fd25994c3454bb048cdb98efca7040f8
sashathor <hello@sashathor.com>
2025-12-18 21:37:29 +0100

    refactor: remove unused exports from TemplateEditor index for cleaner structure

--- packages/react-designer/src/components/TemplateEditor/index.ts
+++ packages/react-designer/src/components/TemplateEditor/index.ts
@@ -1,10 +1,5 @@
 import { Email, Inbox, MSTeams, Push, Slack, SMS } from "./Channels";
 export { BrandFooter } from "@/components/BrandEditor/Editor/BrandFooter";
-export { PreviewPanel } from "@/components/ui/PreviewPanel";
-export { TextMenu } from "@/components/ui/TextMenu";
-export { cn, convertElementalToTiptap, convertTiptapToElemental } from "@/lib/utils";
-export { VariableInput, VariableTextarea } from "@/components/ui/VariableEditor";
-export { getFlattenedVariables } from "@/components/utils/getFlattenedVariables";
 export { useChannels, getChannelDefaults } from "./Channels";
 export { default as EmailEditor } from "./Channels/Email/EmailEditor";
 export * from "./TemplateEditor";
