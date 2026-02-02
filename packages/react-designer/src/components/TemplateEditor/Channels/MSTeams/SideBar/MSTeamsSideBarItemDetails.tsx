fbe715c35443cb4549e87ef150c97038f178bc4d
Geraldo Silva <geraldojuni@hotmail.com>
2026-02-02 11:51:57 -0300

    fix: list element sidebar slack / teams

--- packages/react-designer/src/components/TemplateEditor/Channels/MSTeams/SideBar/MSTeamsSideBarItemDetails.tsx
+++ packages/react-designer/src/components/TemplateEditor/Channels/MSTeams/SideBar/MSTeamsSideBarItemDetails.tsx
@@ -2,6 +2,7 @@ import type { Node as ProseMirrorNode } from "@tiptap/pm/model";
 import type { Editor } from "@tiptap/react";
 import { ButtonForm } from "@/components/extensions/Button";
 import { LinkForm } from "@/components/extensions/Link";
+import { ListForm } from "@/components/extensions/List";
 import { pendingLinkAtom } from "@/components/ui/TextMenu/store";
 import { useAtomValue } from "jotai";
 
@@ -34,6 +35,14 @@ export const MSTeamsSideBarItemDetails = ({
     return (
       <div className="courier-flex courier-flex-col courier-gap-4">
         <ButtonForm element={element} editor={editor} key={element.attrs.id} />
+      </div>
+    );
+  }
+
+  if (element.type.name === "list") {
+    return (
+      <div className="courier-flex courier-flex-col courier-gap-4">
+        <ListForm element={element} editor={editor} key={element.attrs.id} minimalMode />
       </div>
     );
   }
